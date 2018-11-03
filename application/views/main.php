17291bfc6669bd7d0467b218464d2f16f2dc2762
aooen <gkrdus124@gmail.com>
2018-11-04 02:29:30 +0900

    Design banner
    Former-commit-id: 4493e22bf58a46efd1eec548dc1f2987acad6ad9

--- application/views/main.php
+++ application/views/main.php
@@ -7,8 +7,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="container">
 				<div class="banner-content">
 					<h3>포장 제거의 혁신!</h3>
-					랩핑 제거기(Wrapping Remover)를 소개합니다.
-					<a href="<?=base_url('info')?>">자세한 설명 보기</a>
+					랩핑 제거기(Wrapping Remover)를 소개합니다.<br>
+					<a class="btn-link" href="<?=base_url('info')?>">자세한 설명 보기</a>
 				</div>
 			</div>
 		</div>
@@ -16,15 +16,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="container">
 				<div class="banner-content">
 					<h3>타사 제품에 비해 <small>약</small> 2배의 만족도</h3>
-					<a href="<?=base_url('info')?>"></a>
+					<a class="btn-link" href="<?=base_url('info')?>">장점 보기</a>
 				</div>
 			</div>
 		</div>
 		<div id="banner-3" class="banner-element">
 			<div class="container">
 				<div class="banner-content">
-					<h3>대량구매시 더욱 싸게!</h3>
-					<a href="<?=base_url('info')?>">바로 구매</a>
+					<h3>대량 구매시 더욱 싸게!</h3>
+					<a class="btn-link" href="<?=base_url('info')?>">바로 구매</a>
 				</div>
 			</div>
 		</div>
