597cd8b75ffc2df5fb1bf0c66671cabaa739d6c6
aooen <gkrdus124@gmail.com>
2018-11-03 20:44:26 +0900

    메인 틀 작성

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
+	public function _remap($method) {
+		$this->load->view('header.include');
+		if (method_exists($this, $method)) {
+			$this->$method();
+		}
+		$this->load->view('footer.include');
+	}
 
 	public function index()
 	{
---
+++ application/views/header.include.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
