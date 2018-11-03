2fa2de97a83c000459e2915b54eb5006d07ee0b6
aooen <gkrdus124@gmail.com>
2018-11-04 00:36:31 +0900

    Improve animation banner in main view

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,31 @@ var Banner = new function () {
 	this.order = 0;
 	this.$element = $(".banner-element");
 	this.max = this.$element.length;
-	this.next = function () {
-		var next = this.order + 1;
+	this.set = function (i) {
+		this.$element.eq(this.order).animate({display: block, opacity: 0}, 500);
+		this.$element.eq(i).animate({display: block, opacity: 1}, 500);
+		this.order = i;
+	};
+	this.next = function (increment) {
+		if (increment === undefined) increment = 1;
+		var next = this.order + increment;
 		if (next >= this.max) next = 0;
 		this.set(next);
-	};
-	this.set = function (i) {
-		this.$element.eq(this.order).animate({opacity: 0}, 500);
-		this.$element.eq(i).animate({opacity: 1}, 500);
-		this.order = i;
 	};
 };
 
 $(document).ready(function () {
 	setInterval(function () {
 		Banner.next();
-	}, 3000);
+	}, 5000);
+
+	$('.banner-left').click(function () {
+		Banner.next(-1);
+	});
+	$('.banner-right').click(function () {
+		Banner.next();
+	});
+	$('.banner-order-bullet').click(function () {
+		Banner.set($(this).index());
+	});
 });
