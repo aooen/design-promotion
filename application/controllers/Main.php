7a61da66eeb397a4a1366698fbea4dddf53771b7
aooen <gkrdus124@gmail.com>
2018-11-03 20:46:37 +0900

    Main _remap change

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 	public function _remap($method) {
-		$this->load->view('header.include');
+		$this->load->view('header.include.php');
 		if (method_exists($this, $method)) {
 			$this->$method();
 		}
-		$this->load->view('footer.include');
+		$this->load->view('footer.include.php');
 	}
 
 	public function index()
