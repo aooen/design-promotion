c0f342d9eba8a3b5ad38a9866af153fc8bb3bee3
aooen <gkrdus124@gmail.com>
2018-11-03 23:49:54 +0900

    Design banner in main view

--- application/views/main.php
+++ application/views/main.php
@@ -4,26 +4,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="banner-area">
 	<div id="banner-container">
 		<div id="banner-1" class="banner">
-			<h3>가나다</h3>
-			가나다가나다
+			<div class="container">
+				<h3>가나다</h3>
+				가나다가나다
+			</div>
 		</div>
 		<div id="banner-2" class="banner">
-			<h3>가나다</h3>
-			가나다가나다
+			<div class="container">
+				<h3>가나다</h3>
+				가나다가나다
+			</div>
 		</div>
 		<div id="banner-3" class="banner">
-			<h3>가나다</h3>
-			가나다가나다
+			<div class="container">
+				<h3>가나다</h3>
+				가나다가나다
+			</div>
 		</div>
 	</div>
-	<div class="banner-controller">
-		<div class="banner-left"><i class="icon ion-ios-arrow-dropleft-circle"></i></div>
-		<div class="banner-right"><i class="icon ion-ios-arrow-dropright-circle"></i></div>
-		<div class="banner-order">
-			<div  class="banner-order-bullet"></div>
-			<div  class="banner-order-bullet"></div>
-			<div  class="banner-order-bullet"></div>
-		</div>
+	<div class="banner-controller banner-left"><i class="icon ion-ios-arrow-dropleft-circle"></i></div>
+	<div class="banner-controller banner-right"><i class="icon ion-ios-arrow-dropright-circle"></i></div>
+	<div class="banner-controller banner-order">
+		<div class="banner-order-bullet"></div>
+		<div class="banner-order-bullet"></div>
+		<div class="banner-order-bullet"></div>
 	</div>
 </div>
 <div class="container">
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-	
+	setInterval(function () {
+		$('#banner-container').css('left', '-=100%');
+		if ($('#banner-container').css('left') == "-300%") {
+			$('#banner-container').css('left', '0');
+		}
+	}, 500);
 });
