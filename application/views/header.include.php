472f6173e0a23a3971dc9ffb5a3ad81a2ced9308
aooen <gkrdus124@gmail.com>
2018-11-03 23:04:39 +0900

    Improve main view design

--- application/views/header.include.php
+++ application/views/header.include.php
@@ -5,7 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/typeface-nanum-gothic@0.0.63">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/typeface-nanum-square-round@1.0.1">
 	<link rel="stylesheet" href="<?=base_url('static/css/main.css')?>">
 	<title>프로모션</title>
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
@@ -14,12 +15,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 <header>
 	<div class="container">
-		<h1>Lorem</h1>
+		<h1>Wrapping Remover</h1>
 		<nav>
 			<ul>
 				<li>상품 소개</li>
-				<li>장단점</li>
-				<li>상품 구매</li>
+				<li>특·장점</li>
+				<li>바로구매</li>
 				<li>커뮤니티</li>
 			</ul>
 		</nav>
--- application/views/main.php
+++ application/views/main.php
@@ -1,17 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<div id="banner">
-	<div id="banner-1">
-		<h3>가나다</h3>
-		가나다가나다
-	</div>
-	<div id="banner-2">
-		<h3>가나다</h3>
-		가나다가나다
-	</div>
-	<div id="banner-3">
-		<h3>가나다</h3>
-		가나다가나다
+<div class="container">
+	<div id="banner">
+		<div id="banner-1" class="banner-container">
+			<h3>가나다</h3>
+			가나다가나다
+		</div>
+		<div id="banner-2" class="banner-container">
+			<h3>가나다</h3>
+			가나다가나다
+		</div>
+		<div id="banner-3" class="banner-container">
+			<h3>가나다</h3>
+			가나다가나다
+		</div>
+		<div class="banner-controller">
+			<div class="banner-left"></div>
+			<div class="banner-right"></div>
+			<div class="banner-order"></div>
+		</div>
 	</div>
 </div>
