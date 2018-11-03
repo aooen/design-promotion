a782a78cc3272fc8d783b07e111910a0d23a0722
aooen <gkrdus124@gmail.com>
2018-11-03 22:02:01 +0900

    Fix to use base_url

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
+	public function __construct () {
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
 	public function _remap($method) {
 		$this->load->view('header.include.php');
 		if (method_exists($this, $method)) {
