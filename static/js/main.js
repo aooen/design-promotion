77abdd8b3f31e3552e56c71c285efdd9e3b70caf
aooen <gkrdus124@gmail.com>
2018-11-04 04:40:16 +0900

    Fix banner animate
    Former-commit-id: d316e9874305ad76f02c026a63b4d28bae5e812a

--- static/js/main.js
+++ static/js/main.js
@@ -3,22 +3,21 @@ $(document).ready(function () {
 		this.order = 0;
 		this.$element = $(".banner-element");
 		this.max = this.$element.length;
-		this.timer;
-		this.setTimeout = function () {
-			if (this.timer !== undefined) {
-				clearTimeout(this.timer);
+		this.interval;
+		this.setInterval = function () {
+			if (this.interval !== undefined) {
+				clearInterval(this.interval);
 			}
-			this.timer = setTimeout((function () {
-				this.timer = undefined;
+			this.interval = setInterval((function () {
 				this.next();
 			}).bind(this), 5000);
 		};
 		this.set = function (i) {
 			if (this.order == i) return;
-			this.$element.eq(this.order).fadeOut(500);
-			this.$element.eq(i).fadeIn(500);
+			this.$element.eq(this.order).stop().fadeOut(500);
+			this.$element.eq(i).stop().fadeIn(500);
 			this.order = i;
-			this.setTimeout();
+			this.setInterval();
 		};
 		this.next = function (increment) {
 			if (increment === undefined) increment = 1;
@@ -29,7 +28,7 @@ $(document).ready(function () {
 		};
 	};
 
-	Banner.setTimeout();
+	Banner.setInterval();
 
 	$('.banner-left').click(function () {
 		Banner.next(-1);
